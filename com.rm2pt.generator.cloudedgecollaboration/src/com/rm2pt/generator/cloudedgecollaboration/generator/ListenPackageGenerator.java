bb5b6cff5d4ca79822359ce92cdc13d06031850e
evoLonation <1838940019@qq.com>
2023-03-13 18:16:06 +0800

    add gitignore

--- com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/generator/ListenPackageGenerator.java
+++ com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/generator/ListenPackageGenerator.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class ListenPackageGenerator extends Generator{
     public ListenPackageGenerator(List<ServiceInfo> normalServiceList, List<ServiceInfo> mqttServiceList, List<ServiceInfo> httpServiceList) {
-
+        String str1 = "Create table("
+        str1 + ... +
     }
 
     @Override
