0c1e5ea33f811a95f35e5c534b114a16ffd2e529
evoLonation <1838940019@qq.com>
2023-03-16 09:30:42 +0800

    给logable加缩进

--- com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/common/Logable.java
+++ com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/common/Logable.java
@@ -5,21 +5,30 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Logable {
+    static private int tab = 0;
     @Override
     public String toString() {
         Class<?> clazz = getClass();
         Field[] fields = clazz.getDeclaredFields();
+        String tabStr = "\t".repeat(tab);
         String fieldString = Arrays.stream(fields)
                 .map(f -> {
                     f.setAccessible(true);
                     try {
                         Object value = f.get(this);
-                        return f.getName() + "=" + value;
+                        if(! (value instanceof Logable)){
+                            return tabStr + f.getName() + "=" + value;
+                        }else {
+                            tab ++;
+                            String ret = tabStr + f.getName() + ":\n" + value;
+                            tab --;
+                            return ret;
+                        }
                     } catch (IllegalAccessException e) {
                         return f.getName() + "=<ERROR>";
                     }
                 })
-                .collect(Collectors.joining(", \n"));
-        return clazz.getSimpleName() + "{" + fieldString + "}\n";
+                .collect(Collectors.joining("," + tabStr +"\n"));
+        return tabStr + clazz.getSimpleName() + "\n" +  fieldString + "\n";
     }
 }
