0350d89c64cd7d55c9d8652eb79d5d10bd9ec811
MaxLong <longyizhou2001@163.com>
2023-03-23 10:16:04 +0800

    try upodate service factory

--- com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/generator/OperationBodyGenerator.java
+++ com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/generator/OperationBodyGenerator.java
@@ -3,6 +3,7 @@ package com.rm2pt.generator.cloudedgecollaboration.generator;
 import com.rm2pt.generator.cloudedgecollaboration.info.Location;
 import com.rm2pt.generator.cloudedgecollaboration.info.OperationInfo;
 import com.rm2pt.generator.cloudedgecollaboration.info.operationBody.OperationBody;
+import com.rm2pt.generator.cloudedgecollaboration.generator.zzy.ServiceTemplate;
 
 public class OperationBodyGenerator {
     private OperationBody operationBody;
