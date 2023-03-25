22958fe0474c26c316f8156ee9357160e36443d1
Katock <89233261+Katock-Cricket@users.noreply.github.com>
2023-03-25 12:52:20 +0800

    debug，keyTypeMap关键字改为AssoName

--- com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/Processor.java
+++ com.rm2pt.generator.cloudedgecollaboration/src/com/rm2pt/generator/cloudedgecollaboration/Processor.java
@@ -1,4 +1,5 @@
 package com.rm2pt.generator.cloudedgecollaboration;
+
 import com.rm2pt.generator.cloudedgecollaboration.common.Keyworder;
 import com.rm2pt.generator.cloudedgecollaboration.factory.EntityFactory;
 import com.rm2pt.generator.cloudedgecollaboration.factory.GlobalInfoBuilder;
@@ -15,39 +16,42 @@ import org.eclipse.xtext.generator.IFileSystemAccess2;
 public class Processor {
 	private UseCaseModel useCaseModel;
 	private DomainModel domainModel;
+
 	public Processor(Resource resource, IFileSystemAccess2 fsa) {
 		Generator.setFsa(fsa);
 		resource.getAllContents().forEachRemaining(o -> {
-			if(o instanceof UseCaseModel) {
-				this.useCaseModel = (UseCaseModel)o;
+			if (o instanceof UseCaseModel) {
+				this.useCaseModel = (UseCaseModel) o;
 			}
-			if(o instanceof DomainModel) {
-				this.domainModel = (DomainModel)o;
+			if (o instanceof DomainModel) {
+				this.domainModel = (DomainModel) o;
 			}
 		});
 	}
 
-    public void process(){
-    	
-    	System.out.println(Keyworder.camelToUnderScore("MyNameIsZzy"));
+	public void process() {
+
+		System.out.println(Keyworder.camelToUnderScore("MyNameIsZzy"));
 
 		GlobalInfo globalInfo = new GlobalInfoBuilder().build();
-        // 中间代码生成阶段
-        EntityFactory entityFactory = new EntityFactory(domainModel.getEntity());
-        entityFactory.factory();
+		// 中间代码生成阶段
+		EntityFactory entityFactory = new EntityFactory(domainModel.getEntity());
+		entityFactory.factory();
 		new EntityPackageGenerator(entityFactory.getEntityList()).generate();
 		new MysqlGenerator(entityFactory.getEntityList(), globalInfo).generate();
-		ServiceFactory serviceFactory = new ServiceFactory(useCaseModel.getInteraction(), useCaseModel.getService(), useCaseModel.getContract(), entityFactory.getEntityMap());
-        serviceFactory.factory();
+		ServiceFactory serviceFactory = new ServiceFactory(useCaseModel.getInteraction(), useCaseModel.getService(),
+				useCaseModel.getContract(), entityFactory.getEntityMap());
+		serviceFactory.factory();
 		new ServicePackageGenerator(serviceFactory.getServiceList(),
 				serviceFactory.getOperation(OperationInfo.ConcurrencyType.HIGHPRIORITY)).generate();
-		new ConfigGenerator(serviceFactory.getOperation(OperationInfo.ConcurrencyType.HIGHPRIORITY), globalInfo).generate();
+		new ConfigGenerator(serviceFactory.getOperation(OperationInfo.ConcurrencyType.HIGHPRIORITY), globalInfo)
+				.generate();
 		new ServerPackageGenerator(serviceFactory.getServiceList(), globalInfo).generate();
-		new RedisGenerator(serviceFactory.getOperation(OperationInfo.ConcurrencyType.HIGHPRIORITY), globalInfo).generate();
+		new RedisGenerator(serviceFactory.getOperation(OperationInfo.ConcurrencyType.HIGHPRIORITY), globalInfo)
+				.generate();
 
+		// todo configpackage, redisdeploy, commonpackage, serverpackage,
+		// highprioritypackage, etcpackage 's generate
 
-		// todo configpackage, redisdeploy, commonpackage, serverpackage, highprioritypackage, etcpackage 's generate
-
-
-    }
+	}
 }
